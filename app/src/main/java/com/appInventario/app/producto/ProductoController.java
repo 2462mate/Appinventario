1ec50b974af2af8d3a0634ef63728bd8964b4c22
2462mate <nbernadpv@gmail.com>
2023-09-15 13:25:35 -0300

    mi segundo comit

--- app/src/main/java/com/appInventario/app/producto/ProductoController.java
+++ app/src/main/java/com/appInventario/app/producto/ProductoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -42,5 +43,24 @@ public class ProductoController {
         return "productos";
 
     }
+    @GetMapping("/productos/editar/{id}")
+    public String mostrarFormularioDeModificarProducto(@PathVariable("id") Integer id,Model modelo){
+        Producto producto = productoRepository.findById(id).get();
+        modelo.addAttribute("producto",producto);
+
+        List<Categoria> listaCategorias = categoriaRepository.findAll();
+        modelo.addAttribute("listaCategorias", listaCategorias);
+
+
+        return "producto_formulario";
+
+    }
+    @GetMapping("/productos/eliminar/{id}")
+    public String eliminarProducto(@PathVariable("id") Integer id, Model modelo){
+        productoRepository.deleteById(id);
+        return "redirect:/productos";
+
+    }
 
 }
+''
